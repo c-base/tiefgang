4bdb5c5ca78d2d96770825e8b455526f1c480558
Anne-Julia Scheuermann <annejulia@gmail.com>
2015-09-08 11:30:05 +0200

    ripple

--- circleLED.ino
+++ circleLED.ino
@@ -134,7 +134,7 @@ void tickTock(const CRGB &color1, const CRGB &color2, int delayDuration, int rep
         }
 
         leds[leFirstDot] = color1;
-        
+
         FastLED.show();
         allOut();
 
@@ -195,6 +195,81 @@ void rotate(const CRGB &color1, const CRGB &color2, int delayDuration, int repea
 void allOut() {
   for (int dot = 0; dot < NUM_LEDS; dot++) {
     leds[dot] = CRGB::Black;
+  }
+}
+
+
+// Ripple
+//
+// Converted (but not optimized) from the Neopixel version https://gist.github.com/suhajdab/9716635
+//
+// Conversion by Andrew Tuline
+//
+
+void ripple() {
+
+  int color;
+  int center = 0;
+  int step = -1; // need to be parameterized
+  int maxSteps = 16;
+  float fadeRate = 0.8;
+  int diff;
+
+  //background color
+  uint32_t currentBg = random(256);
+  uint32_t nextBg = currentBg;
+
+  if (currentBg == nextBg) {
+    nextBg = random(256);
+  }
+  else if (nextBg > currentBg) {
+    currentBg++;
+  } else {
+    currentBg--;
+  }
+  for (uint16_t l = 0; l < NUM_LEDS; l++) {
+    leds[l] = CHSV(currentBg, 255, 50);         // strip.setPixelColor(l, Wheel(currentBg, 0.1));
+  }
+
+  if (step == -1) {
+    center = random(NUM_LEDS);
+    color = random(256);
+    step = 0;
+  }
+
+  if (step == 0) {
+    leds[center] = CHSV(color, 255, 255);         // strip.setPixelColor(center, Wheel(color, 1));
+    step ++;
+  } else {
+    if (step < maxSteps) {
+      Serial.println(pow(fadeRate, step));
+
+      leds[wrap(center + step)] = CHSV(color, 255, pow(fadeRate, step) * 255);
+      leds[wrap(center - step)] = CHSV(color, 255, pow(fadeRate, step) * 255);
+      if (step > 3) {
+        leds[wrap(center + step - 3)] = CHSV(color, 255, pow(fadeRate, step - 2) * 255);
+        leds[wrap(center - step + 3)] = CHSV(color, 255, pow(fadeRate, step - 2) * 255);
+      }
+      step ++;
+    }
+    else {
+      step = -1;
+    }
+  }
+
+  LEDS.show();
+  delay(50);
+}
+
+
+int wrap(int step) {
+  return step % NUM_LEDS;
+}
+
+
+void one_color_allHSV(int ahue, int abright) {                // SET ALL LEDS TO ONE COLOR (HSV)
+  for (int i = 0 ; i < NUM_LEDS; i++ ) {
+    leds[i] = CHSV(ahue, 255, abright);
   }
 }
 
